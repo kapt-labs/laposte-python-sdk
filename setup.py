3dceb07d6b2d47835ffe7c86d32cb8cb8f51e17c
Guillaume Luchet <gluchet@gmail.com>
2017-03-27 23:25:10 +0200

    Upgrade version [ci skip]

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='lapostesdk',
-    version='0.1',
+    version='0.1.1',
     author='Guillaume Luchet',
     author_email='guillaume@geelweb.org',
     packages=['lapostesdk', 'lapostesdk.apis'],
     url='https://github.com/geelweb/laposte-python-sdk',
-    download_url='https://github.com/geelweb/laposte-python-sdk/archive/0.1.tar.gz',
+    download_url='https://github.com/geelweb/laposte-python-sdk/archive/0.1.1.tar.gz',
     description='La Poste SDK',
     install_requires=[
         'requests>=2.4.3'
