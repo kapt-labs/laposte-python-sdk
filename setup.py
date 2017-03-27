7caa8ab79efab4c7dcfefc4e75bee903435cc725
Guillaume Luchet <gluchet@gmail.com>
2017-03-28 00:15:20 +0200

    update download url [ci skip]

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='guillaume@geelweb.org',
     packages=['lapostesdk', 'lapostesdk.apis', 'lapostesdk.entities'],
     url='https://github.com/geelweb/laposte-python-sdk',
-    download_url='https://github.com/geelweb/laposte-python-sdk/archive/0.1.1.tar.gz',
+    download_url='https://github.com/geelweb/laposte-python-sdk/archive/0.1.2.tar.gz',
     description='La Poste SDK',
     install_requires=[
         'requests>=2.4.3'
